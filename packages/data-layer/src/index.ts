04b8f58c35902cafda45569ba4f7c8c9e0e8083c
Fabio Fognani <fabio.fognani@gmail.com>
2026-05-02 23:23:31 +0200

    removed old package; restart changeset release

--- packages/data-layer/src/index.ts
+++
@@ -1,3 +0,0 @@
-export type { DataLoaderRegistry } from "./create-data-loader-registry";
-export { createDataLoaderRegistry } from "./create-data-loader-registry";
-export { mapBatchToIds } from "./map-batch-to-ids";
