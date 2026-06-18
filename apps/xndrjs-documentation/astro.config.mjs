c703d325a792ffae3cf34d73153d135038bca657
Fabio Fognani <fabio.fognani@gmail.com>
2026-06-18 19:43:55 +0200

    fix: site base url

--- apps/xndrjs-documentation/astro.config.mjs
+++ apps/xndrjs-documentation/astro.config.mjs
@@ -5,7 +5,7 @@ import mermaid from "astro-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://xndrjs.dev",
+  site: "https://www.xndrjs.dev",
   integrations: [
     mermaid(),
     starlight({
